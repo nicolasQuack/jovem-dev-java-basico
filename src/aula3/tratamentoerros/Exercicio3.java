d709becbb7b7dfba721005507f09da992419fe57
nicolasQuack <nicolas.quack@icloud.com>
2024-08-01 00:13:12 -0300

    feat(*): finish exercise 3;

--- src/aula3/tratamentoerros/Exercicio3.java
+++ src/aula3/tratamentoerros/Exercicio3.java
@@ -1,33 +1,29 @@
 package aula3.tratamentoerros;
 
 import java.io.IOException;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.stream.Stream;
+import java.util.Iterator;
 
 public class Exercicio3 {
 	public static void main(String[] args) throws IOException {
-
 		try {
 			divisao(1, 0);
 		} catch (ArithmeticException e) {
 			e.printStackTrace();
 		}
-
 		try {
 			divisao(null, 1);
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
-
 		try {
-			new ArrayList<String>().get(0);
+			"".charAt(0);
 		} catch (StringIndexOutOfBoundsException e) {
 			e.printStackTrace();
 		}
-
 		try {
-				
+			Iterator<String> i = new ArrayList<String>().iterator();
+			i.remove();
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
 		}
